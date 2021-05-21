a88dde60f8a1940d39585e77e6c8467dfb7d3c9a
李白与孟浩然 <OneTaro@users.noreply.github.com>
2021-05-21 21:15:19 +0800

    spring项目

--- src/main/java/com/spring/transaction/Leo006SpringProgrammaticTransactionApplication.java
+++ src/main/java/com/spring/transaction/Leo006SpringProgrammaticTransactionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Leo06SpringProgrammaticTransactionApplication {
+public class Leo006SpringProgrammaticTransactionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Leo06SpringProgrammaticTransactionApplication.class, args);
+        SpringApplication.run(Leo006SpringProgrammaticTransactionApplication.class, args);
     }
 
 }
